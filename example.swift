2f16d0ddb7931d6c3ae6fccfa70b12541e4f03f5
Marcin Majewski <marcin.majewski@sonarsource.com>
2021-02-16 16:15:55 +0100

    4rd commit

--- example.swift
+++ example.swift
@@ -1,6 +1,6 @@
 import Swift
 print("Hello, World!")
 
-func doSomething(param1: String, param2: String, param3: String, param4: String, param5: String) {
+func doSomething(param1: String, param2: String, param3: String, param4: String, param5: String, param6: String, param7: String, param8: String) {
     print("Hello, World 2")
 }
