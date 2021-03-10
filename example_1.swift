7b7812a1653d31c75d83264a4fba5a4ff3308dda
Marcin Majewski <marcin.majewski@sonarsource.com>
2021-03-10 15:05:36 +0100

    Example commit

---
+++ example_1.swift
@@ -0,0 +1,10 @@
+import Swift
+print("Hello, World!")
+
+func doSomething(param1: String, param2: String, param3: String, param4: String, param5: String, param6: String, param7: String, param111: String) {
+    print("Hello, World 2")
+}
+
+func 123() {
+    print("Bla bla")
+}
